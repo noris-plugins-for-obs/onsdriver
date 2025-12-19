noris-plugins-for-obs/onsdriver

commit c0a39423fa777883b1871fc208a8a7c448040ddc

util, obsexec: Implement retry method and use it from obsexec

src/onsdriver/obsexec.py
@@ -10,7 +10,7 @@ import subprocess
 import tempfile
 import time
 import obsws_python
-from onsdriver import obsconfig, obsui
+from onsdriver import obsconfig, obsui, util
 
 _WAIVED_ERRORS_RE_LIST = (
     r'error: Failed to rename basic scene collection file:', # first time
@@ -117,12 +117,18 @@ class OBSExec:
         )
 
         # Wait startup
-        # macos: Sometimes mac-avcapture-legacy takes 5 seconds.
+        # macOS: mac-avcapture-legacy takes up to 5 seconds.
+        # Windows: Sometimes starting EXE takes 10 seconds.
         time.sleep(0.2)
-        retry = 100
-        while retry > 0 and not self._obs_started():
-            time.sleep(0.1)
-            retry -= 1
+        if sys.platform == 'win32':
+            timeout = 25
+            wait = 0.5
+        else:
+            timeout = 10
+            wait = 0.1
+        while util.retry(timeout=timeout, each_wait=wait, error_msg='Checking startup by log'):
+            if self._obs_started():
+                break
 
         cfg = self.config.get_obsws_cfg()
         if 'server_enabled' in cfg and cfg['server_enabled']:
@@ -131,13 +137,10 @@ class OBSExec:
 
             # Ensure the main window is visible,
             # If not, ie. websocket request goes too early, UI will be corrupted.
-            retry += 1
-            while retry > 0:
+            while util.retry(timeout=10, error_msg='Waiting main window is visible'):
                 res = ui.request('widget-list', {})
                 if res['visible']:
                     break
-                time.sleep(0.1)
-                retry -= 1
 
     def _get_obsws_passwd(self):
         cfg = self.config.get_obsws_cfg()

src/onsdriver/util.py
@@ -1,0 +1,25 @@
+'''
+This module provides useful functions when testing with obs-studio.
+'''
+
+import time
+
+def retry(timeout, each_wait=0.1, error_msg=None):
+    '''Retry until meeting a condition
+    Use this function with `while` statement like below.
+    while retry(timeout=10):
+        good = your_task()
+        if good:
+            break
+    :param timeout:    Set the timeout in second
+    :param each_wait:  Sleep time for each
+    '''
+    yield None
+
+    count = int(timeout // each_wait + 0.5)
+    while count > 0:
+        time.sleep(each_wait)
+        yield None
+        count -= 1
+
+    raise TimeoutError(error_msg)

commit c3b6429dfc586cd47646f61213c291a49880bcc3

obsexec, util: Fix not properly wait OBS to start
This fixes a bug introduced by c0a39423fa.

src/onsdriver/obsexec.py
@@ -126,7 +126,7 @@ class OBSExec:
         else:
             timeout = 10
             wait = 0.1
-        while util.retry(timeout=timeout, each_wait=wait, error_msg='Checking startup by log'):
+        for _ in util.retry(timeout=timeout, each_wait=wait, error_msg='Checking startup by log'):
             if self._obs_started():
                 break
 
@@ -137,7 +137,7 @@ class OBSExec:
 
             # Ensure the main window is visible,
             # If not, ie. websocket request goes too early, UI will be corrupted.
-            while util.retry(timeout=10, error_msg='Waiting main window is visible'):
+            for _ in util.retry(timeout=10, error_msg='Waiting main window is visible'):
                 res = ui.request('widget-list', {})
                 if res['visible']:
                     break

src/onsdriver/util.py
@@ -7,7 +7,7 @@ import time
 def retry(timeout, each_wait=0.1, error_msg=None):
     '''Retry until meeting a condition
     Use this function with `while` statement like below.
-    while retry(timeout=10):
+    for _ in retry(timeout=10):
         good = your_task()
         if good:
             break